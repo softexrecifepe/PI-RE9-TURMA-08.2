90c5f9c7c05a13b553a4364ae3a7f409627e249c
Bruno Henrique <brunohenriquef.pinto@gmail.com>
2024-10-16 22:40:15 -0300

    update responsive header

--- re9-fap/src/assets/components/header/Header.tsx
+++ re9-fap/src/assets/components/header/Header.tsx
@@ -1,11 +1,12 @@
 import './header.css';
 import Button from "../buttons/Buttons";
+import { MouseEventHandler } from 'react';
 
-function Burguermenu() {
-    
+function Burguermenu({onClick}: {onClick:MouseEventHandler}) {
+
     return (
         <>
-        <div className="burguer-menu">
+        <div className="burguer-menu" onClick={onClick}>
             <div className="line-buguer-menu"></div>
             <div className="line-buguer-menu"></div>
             <div className="line-buguer-menu"></div>
@@ -16,16 +17,30 @@ function Burguermenu() {
 
 
 function Header() {
+
+    function handleClickmenu() {
+        const menu = document.querySelector("ul.menu-header")
+        menu?.classList.toggle("menu-header-visible");
+    }
     
     return (
         <>
             <header>
                 <nav className="nav-bar">
-                <div className="left-header"><Burguermenu/></div>
+                <div className="left-header">
+                    <Burguermenu 
+                    onClick={handleClickmenu}
+                    />
+                    <ul className='menu-header'>
+                        <li>Login</li>
+                        <li>Cadastre-se</li>
+                    </ul>
+                </div>
                 <div className="center-header">
                     <div className="logo"></div>
                 </div>
                 <div className="right-header">
+                    <div className='logo-right'>LOGO</div>
                     <div className="buttons-container">
                         <Button stylebutton="primary-button" text="Entrar"/>
                         <Button stylebutton="secundary-button" text="Cadastre-se"/>
