0cfe83b915f182352bfadcaf7d8dd2819ec1d196
Bruno Henrique <brunohenriquef.pinto@gmail.com>
2024-10-17 17:21:05 -0300

    add animação no menu hamburguer

--- re9-fap/src/assets/components/header/Header.tsx
+++ re9-fap/src/assets/components/header/Header.tsx
@@ -7,9 +7,9 @@ function Burguermenu({onClick}: {onClick:MouseEventHandler}) {
     return (
         <>
         <div className="burguer-menu" onClick={onClick}>
-            <div className="line-buguer-menu"></div>
-            <div className="line-buguer-menu"></div>
-            <div className="line-buguer-menu"></div>
+            <div className="line-buguer-menu" id='line-1'></div>
+            <div className="line-buguer-menu" id='line-2'></div>
+            <div className="line-buguer-menu" id='line-3'></div>
         </div>
         </>
     )
@@ -20,7 +20,23 @@ function Header() {
 
     function handleClickmenu() {
         const menu = document.querySelector("ul.menu-header")
-        menu?.classList.toggle("menu-header-visible");
+        const burguerMenu = document.querySelector("div.burguer-menu")
+
+        const line1 = burguerMenu?.querySelector("div#line-1")
+        const line2 = burguerMenu?.querySelector("div#line-2")
+        const line3 = burguerMenu?.querySelector("div#line-3")
+
+        if (menu?.classList.contains("menu-header-visible")) {
+            menu?.classList.remove("menu-header-visible");
+            line1?.classList.remove("active")
+            line2?.classList.remove("active")
+            line3?.classList.remove("active")
+        } else {
+            menu?.classList.add("menu-header-visible");
+            line1?.classList.add("active")
+            line2?.classList.add("active")
+            line3?.classList.add("active")
+        }
     }
     
     return (
