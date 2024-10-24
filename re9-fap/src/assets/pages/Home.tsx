b289d0fc9b07d49eed14d6f280cc0ae526f8df0e
Bruno Henrique <129626387+obrunohenrique@users.noreply.github.com>
2024-10-24 13:47:27 +0000

    update develop - ainda falta muita coisa

--- re9-fap/src/assets/pages/Home.tsx
+++ re9-fap/src/assets/pages/Home.tsx
@@ -1,12 +1,16 @@
 import Header from "../components/header/Header";
 import Card from "../components/card-inicial/Card";
+import CardComentarios from "../components/card-comentarios/Card-comentarios";
 
 function Home() {
     
     return (
         <>
             <Header/>
+        <main>
             <Card/>
+            <CardComentarios/>
+        </main>  
         </>
     )
 }
