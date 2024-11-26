e1ef2405932159e563aa64957d1079cc0d380cda
KarolynneFreire <karolynnefreire@gmail.com>
2024-11-25 22:11:33 -0300

    incluindo documentação nos componentes da home

--- re9-fap/src/assets/components/cardcadastro/Cardcadastro.tsx
+++ re9-fap/src/assets/components/cardcadastro/Cardcadastro.tsx
@@ -1,8 +1,12 @@
-import './cardcadastro.css';
-import layoutcadastro from '../../img/layout-cadastroo.svg';
-import { Link } from 'react-router-dom';
-import Button from "../buttons/Buttons";
+import './cardcadastro.css'; 
+import layoutcadastro from '../../img/layout-cadastroo.svg'; 
+// import { useNavigate } from 'react-router-dom'; 
+import Button from "../buttons/Buttons"; 
 
+/**
+ * Componente funcional que renderiza dois cards para cadastro, um para empresas e outro para instituições.
+ * Cada card contém uma imagem, um título e um botão que redireciona para uma rota específica.
+ */
 const Cardcadastro: React.FC = () => {
   return (
     <div className="container-cadastro">
@@ -10,23 +14,33 @@ const Cardcadastro: React.FC = () => {
         <h2>Realize seu cadastro</h2>
       </div>
       <div className="card-container">
+        {/* Card para empresas */}
         <div className="card-cadastro">
           <div className="layout-cadastro">
             <img src={layoutcadastro} alt="Layout" />
           </div>
           <div className="empresa-instituicao-botao">
             <h1>Empresa</h1>
-            <Button stylebutton="secundary-button" text="Clique Aqui" />
+            {/* Botão que navega para a rota de cadastro de empresas */}
+            <Button
+              stylebutton="secundary-button marge-botton"
+              text="Clique Aqui"
+            />
           </div>
         </div>
-        
+
+        {/* Card para instituições */}
         <div className="card-cadastro">
           <div className="layout-cadastro">
             <img src={layoutcadastro} alt="Layout" />
           </div>
           <div className="empresa-instituicao-botao">
             <h1>Instituição</h1>
-            <Button stylebutton="secundary-button" text="Clique Aqui" />
+            {/* Botão que navega para a rota de cadastro de instituições */}
+            <Button
+              stylebutton="secundary-button marge-botton"
+              text="Clique Aqui"
+            />
           </div>
         </div>
       </div>
@@ -34,7 +48,4 @@ const Cardcadastro: React.FC = () => {
   );
 };
 
-
-
 export default Cardcadastro;
-
